babf83b7f35d09c27509a7f339ba61ddf7708e57
Benediktus Anindito <bennybroz105@gmail.com>
2023-11-14 17:58:08 +0000

    bug fixes; change http verbs for create action

--- employee_api/router.py
+++ employee_api/router.py
@@ -2,6 +2,7 @@
 
 from fastapi import Depends, APIRouter, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 
 from . import crud, models, schemas
 from .config import SessionLocal, engine, Base
@@ -32,13 +33,26 @@ async def get_employee(emp_id: int, session: Session = Depends(get_db)) -> schem
         raise HTTPException(status_code=404, detail='employee not found')
     return schemas.Employee.from_orm(empl)
 
-@emprouter.post("/employees")
+@emprouter.put("/employees")
 async def new_employee(emp: schemas.EmployeeBase, session: Session = Depends(get_db)) -> schemas.Employee:
-    empl = crud.create_employee(session, emp) 
-    if empl is None:
-        raise HTTPException(status_code=404, detail='employee not found')
+    empl = None
+    try:
+        empl = crud.create_employee(session, emp) 
+    except IntegrityError as e:
+        raise HTTPException(status_code=403, detail='email already registered')
     return schemas.Employee.from_orm(empl)
 
 @emprouter.patch("/employees/{emp_id}")
 async def update_employee(emp_id: int, emp: schemas.EmployeeUpdate, session: Session = Depends(get_db)):
-    crud.update_employee(session, emp_id, emp)
+    try:
+        crud.update_employee(session, emp_id, emp)
+    except IntegrityError as e:
+        raise HTTPException(status_code=403, detail='email already registered')
+
+@emprouter.delete("/employees/{emp_id}")
+async def delete_employee(emp_id: int, session: Session = Depends(get_db)):
+    empl = crud.get_employee_by_id(session, emp_id)
+    if empl is None:
+        raise HTTPException(status_code=404, detail='employee not found')
+    session.delete(empl)
+    session.commit()
